0b9c27cf1df4d2cd87f4a7c0a6bb706d9651572f
shaw0lee0 <robo_one@foxmail.com>
2015-10-20 06:42:16 +0800

    updata main.py

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -2,6 +2,30 @@
 # version 1.0
 # Author:robo_one
 # Function:this script is a diary editor
+# read,write,open,print
+#encoding=utf-8  
 
 
+from sys import argv
+
+#diary_write, diary_read, diary_name = argv
+
+import datetime
+
+now = datetime.datetime.now()
+
+otherStyleTime = now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def diary_write ():
+	while Ture:
+		print "%r\r\n " % otherStyleTime
+		txt = raw_input(otherStyleTime)
+		if len(txt) < 2 :
+			print ("Too short txt")
+		elif len(txt) > 2 :
+			
+		return txt
+				break
+diary_write()
 
