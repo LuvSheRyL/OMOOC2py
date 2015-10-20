4c1dbee0cf476960f8ea7cddf1b90ad5033150f3
shawn0lee0 <robo_one@foxmail.com>
2015-10-20 16:53:34 +0800

    updata main().py,and add diary.txt

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,31 +1,49 @@
-# 2015-10-18 22:21:58
+# 2015-10-20 15:51:50
 # version 1.0
 # Author:robo_one
 # Function:this script is a diary editor
 # read,write,open,print
-#encoding=utf-8  
+# -*- coding: utf-8 -*-
 
 
 from sys import argv
+script , input_file = argv
 
-#diary_write, diary_read, diary_name = argv
+current_file = open(input_file, 'a')
 
+# import sys data,time
 import datetime
-
 now = datetime.datetime.now()
-
 otherStyleTime = now.strftime("%Y-%m-%d %H:%M:%S")
+#read diary
+def diary_read(f):
+	print "Your file %r:" % input_file
+	f.seek(0,0)
+	print current_file.read()
+	current_file.close()
 
+# function write
+def diary_write(f):
+	line = raw_input("-->")
+	current_file.write("Time %r  \r\n" % otherStyleTime)
+	current_file.write(line)
+	current_file.write("\n")
+	
+	print "Save,we'll close it **** "
+	current_file.close()
 
-def diary_write ():
-	while Ture:
-		print "%r\r\n " % otherStyleTime
-		txt = raw_input(otherStyleTime)
-		if len(txt) < 2 :
-			print ("Too short txt")
-		elif len(txt) > 2 :
-			
-		return txt
-				break
-diary_write()
+def diary_main():
+	print "***Press 'w' or 'W' write diary.********\n"
+	print "***Press 'r' or 'R' read all diary.*****\n"
+	a = raw_input("?")
+	if a=='w' or a=='W':
+		diary_write(current_file)
+	elif a=='r' or a=='R':
+		diary_read(current_file)
+	else:
+		print "Error,plz input w or r?"
+					
 
+while 1:
+	diary_main()
+	
---
+++ _src/om2py0w/0wex1/readme.md
@@ -0,0 +1,21 @@
+﻿# 日记本
++ 一次接收输入一行日记
++ 保存为本地文件
++ 再次运行系统时,能打印出过往的所有日记
+
+
+----------
+
+
+>file命令使用参考：[python files io](http://www.tutorialspoint.com/python/python_files_io.htm)
+
+>read,write命令使用参考：[LPTHW-ex20](http://old.sebug.net/paper/books/LearnPythonTheHardWay/ex20.html)
+
+>调用系统时间命令参考：[python操作日期和时间的方法](http://www.jb51.net/article/47957.htm)
+
+>中文编码调用参考：[中文编码调用](http://ju.outofmemory.cn/entry/26092)
+
+>Python程序输出到文件中：[Python程序输出到文件中](http://www.cnblogs.com/sysuoyj/archive/2012/03/14/2395868.html)
+
+思路很混乱
+各处代码东拼西凑。
