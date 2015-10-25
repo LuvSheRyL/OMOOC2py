fe123a5781fc1bda9c9602939265601b62f14d83
shaw0lee0 <robo_one@foxmail.com>
2015-10-25 15:28:46 +0800

    updata

--- _src/om2py2w/2wex0/main.py
+++ _src/om2py2w/2wex0/main.py
@@ -1,37 +1,57 @@
-#!/usr/bin/env python
-#-*- encoding:utf-8 -*-
-#一个日记本的GUI程序
-#作者：robo_one
-#日期：2015年10月23日
-#版本：V1.0
+#!/bin/env python
+# coding=utf-8
 
-#A Simple Hello World Program
+'''A simple sentence to describe the function of this module.
+
+Add detailed description here if need.
+
+Eample:
+    # python ./sample.py -t
+    # ...
+'''
+# 导入Tk GUI模块
 from Tkinter import *
+from os.path import exists
+from ScrolledText import *
+import tkMessageBox
+from tkFileDialog import *
+import fileinput
+import time
 
-class Application(Frame):
-    def say_hi(self):
-        print "hi there, everyone!"
+# Can be 'Prototype', 'Development', 'Product'
+__status__ = 'Development'
+__author__ = 'robo_one <robo_one@foxmail.com>'
 
-    def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["fg"]   = "red"
-        self.QUIT["command"] =  self.quit
-
-        self.QUIT.pack({"side": "left"})
-
-        self.hi_there = Button(self)
-        self.hi_there["text"] = "Hello",
-        self.hi_there["command"] = self.say_hi
-
-        self.hi_there.pack({"side": "left"})
-
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
-
+# Add your code here
+# Delete the description when you start coding.
+			
+class diary(Frame):
+	def diary_widgets(self):
+		self.open = Button(self)
+		self.open["text"] = "打开文件"
+		self.open["fq"] = "Blue"
+		self.open["command"] = self.diary_read_print
+		self.open.pack({"side":"left"})
+	
+	self.st=ScrolledText(self.f,background="white")
+	self.st.pack(side=LEFT,fill=BOTH,expand=1)
+	
+	def diary_read_print(self):
+	 p1=END
+	 oname=askopenfilename(filetypes=[("Python file","*.*")])
+	  if oname:
+		for line in fileinput.input(oname):
+			self.st.insert(p1,line)
+	   self.t.title(oname)
+	
 root = Tk()
-app = Application(master=root)
+app = diary(master=root)
 app.mainloop()
-root.destroy()
+root.destory()
+	
+	
+
+if __name__ == '__main__':
+    main()
+
+# vim: set expandtab smarttab shiftwidth=4 tabstop=4:
